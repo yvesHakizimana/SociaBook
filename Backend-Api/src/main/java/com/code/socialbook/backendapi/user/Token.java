de420b0d277b1005a730164ece2161be88e49c04
Yves HAKIZIMANA <yvhakizimana123@gmail.com>
2024-04-26 12:18:14 +0200

    Token Model

---
+++ Backend-Api/src/main/java/com/code/socialbook/backendapi/user/Token.java
@@ -0,0 +1,29 @@
+package com.code.socialbook.backendapi.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Token {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String token;
+    private LocalDateTime createdAt;
+    private LocalDateTime expiresAt;
+    private LocalDateTime validatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "userId", nullable = false)
+    private User user;
+}
