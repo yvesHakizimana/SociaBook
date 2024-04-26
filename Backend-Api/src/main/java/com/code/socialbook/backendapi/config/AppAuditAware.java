e17ded9a4048df49e6d6e63e9bfcd3b39ac174ea
Yves HAKIZIMANA <yvhakizimana123@gmail.com>
2024-04-26 15:56:55 +0200

    Implementing Custom Auditing Feature.

---
+++ Backend-Api/src/main/java/com/code/socialbook/backendapi/config/AppAuditAware.java
@@ -0,0 +1,22 @@
+package com.code.socialbook.backendapi.config;
+
+import com.code.socialbook.backendapi.user.User;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+
+public class AppAuditAware implements AuditorAware<Integer> {
+    @Override
+    public Optional<Integer> getCurrentAuditor() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if(auth == null || !auth.isAuthenticated() || auth instanceof AnonymousAuthenticationToken) {
+            return Optional.empty();
+        }
+
+        User userPrincipal = (User) auth.getPrincipal();
+        return Optional.ofNullable(userPrincipal.getId());
+    }
+}
