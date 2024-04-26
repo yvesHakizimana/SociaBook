219b0563096dc3c38503e5c6c175b0b5d36bd376
Yves HAKIZIMANA <yvhakizimana123@gmail.com>
2024-04-26 12:18:32 +0200

    User Repository.

---
+++ Backend-Api/src/main/java/com/code/socialbook/backendapi/user/UserRepository.java
@@ -0,0 +1,10 @@
+package com.code.socialbook.backendapi.user;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByEmail(String username);
+}
