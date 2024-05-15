a199a59d3409adc283e3074f962e25d67304333f
Yves HAKIZIMANA <yvhakizimana123@gmail.com>
2024-05-15 21:37:18 +0200

    Activate Account Functionality.

--- book-network-ui/src/app/pages/activate-account/activate-account.component.ts
+++ book-network-ui/src/app/pages/activate-account/activate-account.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
+import { Router } from "@angular/router";
+import { AuthenticationService } from "../../services/services/authentication.service"
 
 @Component({
   selector: 'app-activate-account',
   templateUrl: './activate-account.component.html',
-  styleUrl: './activate-account.component.scss'
+  styleUrls: ['./activate-account.component.scss']
 })
 export class ActivateAccountComponent {
+  message: string = '';
+  isOkay: boolean = true;
+  submitted: boolean = false;
 
+  constructor(private router: Router, private authService: AuthenticationService) {}
+
+  onCodeCompleted(token: string) {
+    this.confirmAccount(token);
+  }
+
+  redirectToLogin() {
+    this.router.navigate(['login']);
+  }
+
+  private confirmAccount(token: string) {
+    this.authService.confirm({ token }).subscribe({
+      next: () => {
+        this.message = "Your account has been successfully activated. Now you can proceed to login.";
+        this.submitted = true;
+      },
+      error: (err) => {
+        if (err.error) {
+          this.message = "The token has been expired or invalid.";
+          this.submitted = false;
+          this.isOkay = false;
+        }
+      }
+    });
+  }
 }
