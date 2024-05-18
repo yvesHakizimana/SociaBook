39bc1438aec1095665ec3a692326895e16dd86a5
Yves HAKIZIMANA <yvhakizimana123@gmail.com>
2024-05-18 19:00:14 +0200

    Code Refactoring.

--- book-network-ui/src/app/modules/book/book.module.ts
+++ book-network-ui/src/app/modules/book/book.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {CommonModule, NgOptimizedImage} from '@angular/common';
 
 import { BookRoutingModule } from './book-routing.module';
+import { MainComponent } from './pages/main/main.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { BookListComponent } from './pages/book-list/book-list.component';
+import { BookCardComponent } from './components/book-card/book-card.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MainComponent,
+    MenuComponent,
+    BookListComponent,
+    BookCardComponent
+  ],
   imports: [
     CommonModule,
-    BookRoutingModule
+    BookRoutingModule,
   ]
 })
 export class BookModule { }
