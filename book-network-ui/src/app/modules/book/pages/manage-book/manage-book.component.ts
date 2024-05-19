e6d47145fc537e7dfe523eda7ac6c125e38a3013
Yves HAKIZIMANA <yvhakizimana123@gmail.com>
2024-05-19 12:16:43 +0200

    Created the form for capturing the book data to be saved according to a connected to user.

--- book-network-ui/src/app/modules/book/pages/manage-book/manage-book.component.ts
+++ book-network-ui/src/app/modules/book/pages/manage-book/manage-book.component.ts
@@ -1,18 +1,26 @@
 import { Component } from '@angular/core';
+import {BookRequestDto} from "../../../../services/models/book-request-dto";
+import {saveBook} from "../../../../services/fn/book/save-book";
+import {BookService} from "../../../../services/services/book.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-manage-book',
   templateUrl: './manage-book.component.html',
   styleUrl: './manage-book.component.scss'
 })
-export class ManageBookComponent {
+export class ManageBookComponent  {
   errorMsg: string[] = [];
   selectedBookCover: any;
   selectedPicture: string | undefined;
+  bookRequest : BookRequestDto = {title: "", authorName: "", isbn: "", synopsis: "", shareable: false};
+
+  constructor(private bookService: BookService, private router: Router){
+
+  }
 
   onFileSelected(event: any) {
     this.selectedBookCover = event.target.files[0];
-    console.log(this.selectedBookCover);
     //Fetching and reloading the page to display the selected image.
     if(this.selectedBookCover){
       //Reading a file
@@ -22,5 +30,31 @@ export class ManageBookComponent {
       }
       reader.readAsDataURL(this.selectedBookCover);
     }
+  }
+
+  saveBook() {
+    this.bookService.saveBook({
+      body: this.bookRequest
+    }).subscribe({
+      next: (bookId) => {
+        this.bookService.uploadBookCoverPicture({
+          "book-id": bookId,
+          body: {
+            file: this.selectedBookCover,
+          }
+        }).subscribe({
+          next: () => {
+            this.router.navigate(['/books/my-books']);
+          },
+          error: (err) => {
+            console.log(err.error);
+          }
+        })
+      },
+      error: (err) => {
+        this.errorMsg = err.error["validationErrors"];
+        console.log(err);
+      }
+    })
   }
 }
