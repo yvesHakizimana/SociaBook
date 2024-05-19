85d13508859e47d2567527be964f9a9542567ae0
Yves HAKIZIMANA <yvhakizimana123@gmail.com>
2024-05-19 10:20:04 +0200

    Adding borrowing book feature UI.

--- book-network-ui/src/app/modules/book/pages/book-list/book-list.component.ts
+++ book-network-ui/src/app/modules/book/pages/book-list/book-list.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
 import {BookService} from "../../../../services/services/book.service";
 import {PageResponseBookResponse} from "../../../../services/models/page-response-book-response";
+import {borrowBook} from "../../../../services/fn/book/borrow-book";
+import {BookResponse} from "../../../../services/models/book-response";
 
 @Component({
   selector: 'app-book-list',
@@ -12,6 +14,8 @@ export class BookListComponent  implements OnInit{
   page = 0;
   size = 5;
   bookResponse: PageResponseBookResponse = {};
+  message = "";
+  level = "success";
 
 
   constructor(private router: Router, private bookService: BookService) {
@@ -62,5 +66,22 @@ export class BookListComponent  implements OnInit{
 
   get isLastPage(){
     return this.page == this.bookResponse.totalPages as number - 1;
+  }
+
+  borrowBook(book: BookResponse) {
+    this.bookService.borrowBook({
+      'book-id': book.id as number
+    }).subscribe({
+      next: () => {
+        this.message = "The book borrowed successfully"
+        this.level = "success"
+      },
+      error: (err) =>{
+        if(err.error){
+          this.level = "error"
+          this.message = err.error["error"];
+        }
+    }
+    })
   }
 }
