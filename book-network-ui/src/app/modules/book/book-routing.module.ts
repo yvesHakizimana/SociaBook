20379f1c9e8748ab73c55e66d0dffb8030fc7d5f
Yves HAKIZIMANA <yvhakizimana123@gmail.com>
2024-05-19 17:57:03 +0200

    Code Refactoring.

--- book-network-ui/src/app/modules/book/book-routing.module.ts
+++ book-network-ui/src/app/modules/book/book-routing.module.ts
@@ -6,11 +6,13 @@ import {MyBooksComponent} from "./pages/my-books/my-books.component";
 import {ManageBookComponent} from "./pages/manage-book/manage-book.component";
 import {BorrowedBooksComponent} from "./pages/borrowed-books/borrowed-books.component";
 import {ReturnedBooksComponent} from "./pages/returned-books/returned-books.component";
+import {authGuard} from "../../services/guard/auth.guard";
 
 const routes: Routes = [
   {
     path: "",
     component: MainComponent,
+    canActivate: [authGuard],
     children: [
       {
         path: "",
@@ -20,22 +22,27 @@ const routes: Routes = [
   },
   {
     path: "my-books",
+    canActivate: [authGuard],
     component: MyBooksComponent
   },
   {
     path: "manage",
+    canActivate: [authGuard],
     component: ManageBookComponent
   },
   {
     path: "manage/:bookId",
+    canActivate: [authGuard],
     component: ManageBookComponent
   },
   {
     path: "my-borrowed-books",
+    canActivate: [authGuard],
     component: BorrowedBooksComponent
   },
   {
     path: "my-returned-books",
+    canActivate: [authGuard],
     component: ReturnedBooksComponent
   }
 ];
