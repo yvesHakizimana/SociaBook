15396ad4797da5634621073b776d52efc5f60251
Yves HAKIZIMANA <yvhakizimana123@gmail.com>
2024-05-27 23:29:21 +0200

    Re-Mapping the server port of the backend.

--- Backend-Api/src/main/java/com/code/socialbook/backendapi/config/OpenApiConfig.java
+++ Backend-Api/src/main/java/com/code/socialbook/backendapi/config/OpenApiConfig.java
@@ -29,7 +29,7 @@ import io.swagger.v3.oas.annotations.servers.Server;
         servers = {
                 @Server(
                         description = "Local ENV",
-                        url = "http://localhost:8080/api/v1"
+                        url = "http://localhost:8090/api/v1"
                 ),
                 @Server(description = "Prod ENV", url = "http://rca.ac.rw/testing")
         },
